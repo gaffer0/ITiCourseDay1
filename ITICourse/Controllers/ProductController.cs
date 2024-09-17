ebb7123f5e612e2b79e2eb22924e7c4ef6faff14
AhmedGaffer <ahmedgaffer510@gmail.com>
2024-09-17 16:43:54 +0200

    ITI MVC course Day1

---
+++ ITICourse/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using ITICourse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITICourse.Controllers
+{
+    public class ProductController : Controller
+    {
+        // 1. Actions can't be private
+        // 2. Actions can't be static
+        // 3. Actions can't be overloaded
+
+        // Types of action results:
+        // 1. ContentResult - returns a string
+        // 2. ViewResult - returns an HTML view
+        // 3. JavaScriptResult - returns JavaScript (not directly available in ASP.NET Core)
+        // 4. JsonResult - returns JSON
+        // 5. NotFoundResult - returns a 404 status
+        // 6. FileResult - returns a file
+
+        // In JSON, the browser returns all keys in lowercase
+
+
+
+        //product/showmix/1?age=44
+        //product/showmix?id=2&age=44
+
+
+
+
+        /*
+         *   public IActionResult ShowMix(int id,int age)
+        {
+            if (id % 2 == 0)
+            {
+                ContentResult contentResult = new ContentResult();
+                contentResult.Content = "local Msg";
+                return contentResult;
+            }
+            else
+            {
+                ViewResult result = new ViewResult();
+                result.ViewName = "View1";
+                return result;
+            }
+        }
+
+        public JsonResult ShowJson()
+        {
+            JsonResult result = new JsonResult(new { ID = 1, Name = "ahmed" });
+            return result;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+     
+         */
+        //product/showmg
+        public string ShowMG()
+        {
+            return "hello from first action";
+        }
+
+        public IActionResult Details(int id)
+        {
+            // Initialize the sample data class and retrieve the product by id
+            ProductSampleData sampleData = new ProductSampleData();
+            Product productModel = sampleData.GetById(id);
+
+            // Send the retrieved product model to the "ProductDetails" view
+            return View("ProductDetails", productModel);
+        }
+
+        ProductSampleData sampledata=new ProductSampleData();
+
+        public ActionResult index()
+        {
+            List<Product> productListModel = sampledata.GetAll();
+            return View("ShowAll",productListModel);//typeModel list,product>
+        }
+
+
+
+
+    }
+}
---
+++ ITICourse/Models/ProductSampleData.cs
@@ -0,0 +1,28 @@
+﻿namespace ITICourse.Models
+{
+    public class ProductSampleData
+    {
+        List<Product> products;
+
+        public ProductSampleData()
+        {
+            products = new List<Product>();
+            products.Add(new Product() { Id = 1, Name = "IPhone", Price = 10000, Image = "1.jpg" });
+            products.Add(new Product() { Id = 2, Name = "IPhone2", Price = 20000, Image = "1.jpg" });
+            products.Add(new Product() { Id = 3, Name = "IPhone3", Price = 30000, Image = "1.jpg" });
+            products.Add(new Product() { Id = 4, Name = "IPhone4", Price = 40000, Image = "1.jpg" });
+        }
+
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product GetById(int id)
+        {
+            return products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+
+   
+}
